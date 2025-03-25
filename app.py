a1d113d787c4b9e885db85dfcd5e4258b79c34e2
ItsMeNengYi <20040408gry@gmail.com>
2025-03-25 17:09:20 +0800

    Implement database that store user chat history under database/ folder
    dataframes generated by pandasai is stored under database/chat_<index>/dataframes
    files uploaded by user is stored under database/chat_<index>/dataframes
    messages and other information is stored under database/chat_<index>/data.json
    pandasai generated chart cannot be saved under database/ folder
    bug fix: error when response generated by pandasai is not graph nor dataframe

--- app.py
+++ app.py
@@ -4,20 +4,20 @@ import os
 import pandasai as pdai
 import pandas as pd
 from pandasai_openai import OpenAI
+from database import Database
 
 # Load the API KEY 
 load_dotenv() 
 OPENAI_API_KEY = os.environ.get("OPENAI_API_KEY", "")
 PANDASAI_API_KEY = os.environ.get("PANDASAI_API_KEY", "")
 
-# Create directory for the data
-if not os.path.exists("data"):
-    os.makedirs("data")
-
 # Setup variables
 dfs = {} # {path : Dataframe}
 selected_df = None # Selected {path : dataframe}
 num_of_rows = 1
+if "database" not in st.session_state:
+    st.session_state["database"] = Database()
+
 if "messages" not in st.session_state:
     display_message = ""
     if len(dfs) == 0:
@@ -26,25 +26,33 @@ if "messages" not in st.session_state:
         display_message = "What would you like to know about this data?"
     st.session_state["messages"] = [{"role": "assistant", "content": display_message}]
 
-# Choose LLM
+# Load the data
+dfs, selected_df, st.session_state["messages"] = st.session_state["database"].load()
+
+# PandasAI Setup
 if OPENAI_API_KEY != "":
     llm = OpenAI(api_token=OPENAI_API_KEY) # OpenAI model
     pdai.config.set({"llm": llm})
 else:
     pdai.api_key.set(PANDASAI_API_KEY)
 
+# Redirect generated charts to the database folder
+# pdai.config.set({"save_charts_path" : st.session_state["database"].get_exports_path()})
+
+
 # Titles
 st.title("CyberSierra Chatbot")
-st.caption("powered by OpenAI")
+st.caption("powered by OpenAI and PandasAI")
 
 # File Uploader
-uploaded_files = st.file_uploader(type=["csv", "xls"],accept_multiple_files=True, label="Upload a csv")
+uploaded_files = st.file_uploader(type=["csv", "xls"], accept_multiple_files=True, label="Upload csv or xls files")
 if uploaded_files is not None:
     for file in uploaded_files:
-        # Save the file under the data directory
-        with open(os.path.join("data", file.name), "wb") as f:
+        # Save the file in data folder
+        files_folder_path = st.session_state["database"].get_saved_file_path()
+        with open(os.path.join(files_folder_path, file.name), "wb") as f:
             f.write(file.getbuffer())
-        path = os.path.join("data", file.name)
+        path = os.path.join(files_folder_path, file.name)
         # Load the data into dataframe
         if file.name.endswith(".xls"):
             dfs.update({file.name : pd.read_excel(path)})
@@ -71,7 +79,6 @@ if selected_df is not None:
 
 # response parser
 def parse_and_save(response):
-
     if not isinstance(response.type, list):
         response.type = [response.type]
         response.value = [response.value]
@@ -79,13 +86,13 @@ def parse_and_save(response):
     for index,t in enumerate(response.type):
         if t == 'dataframe':
             df = response.value[index]
-            res = {"role": "assistant", "type": "dataframe", "content": df}
-        elif t == "chart" or "plot" or "graph":
+            path = st.session_state["database"].save_dataframe(df)
+            res = {"role": "assistant", "type": "dataframe", "content": path}
+        elif t == "chart" or t == "plot":
             path = response.value[index]
             res = {"role": "assistant", "type": "plot", "content": path}
         else:
-            print("this is "+ t)
-            res =  {"role": "assistant", "content": response.value[index]}
+            res =  {"role": "assistant", "content": str(response.value[index])}
         st.session_state.messages.append(res)
 
 # Displaying Message
@@ -95,14 +102,13 @@ for msg in st.session_state.messages:
     if "type" in msg:
         if msg["type"] == "dataframe":
             st.chat_message(msg["role"]).write("Displaying the dataframe")
-            st.chat_message(msg["role"]).write(pd.DataFrame(msg["content"]))
+            st.chat_message(msg["role"]).write(pd.read_csv(msg["content"]))
+            continue
         elif msg["type"] == "plot":
             st.chat_message(msg["role"]).write("Displaying the plot")
             st.image(msg["content"])
-        else:
-            st.chat_message(msg["role"]).write(msg["content"])
-    else:
-        st.chat_message(msg["role"]).write(msg["content"])
+            continue
+    st.chat_message(msg["role"]).write(msg["content"])
 
 # Process prompts
 if prompt := st.chat_input("Ask a question about " + (str(selected_df[0]) if  selected_df is not None else "") + " data"):
@@ -119,3 +125,9 @@ if prompt := st.chat_input("Ask a question about " + (str(selected_df[0]) if  se
 
     # Update the chatbot response
     st.rerun()
+
+# Save the data
+if len(dfs) > 0:
+    st.session_state["database"].save(dfs_paths=list(dfs.keys()),
+                                    selected_df_path = selected_df[0] if selected_df is not None else None, 
+                                    messages=st.session_state["messages"])
