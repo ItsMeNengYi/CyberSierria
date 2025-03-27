7dfc22a80b7ca2099b9f3a37371c0f1eca7ffe80
ItsMeNengYi <20040408gry@gmail.com>
2025-03-27 19:13:13 +0800

    Implement user password recommendation

--- pages/signup.py
+++ pages/signup.py
@@ -1,17 +1,26 @@
 import streamlit as st
 from auth import auth
 from database import database
+from zxcvbn import zxcvbn
 
 def main():
     st.title("Create a New Account")
 
     username = st.text_input("Username")
     password = st.text_input("Password", type='password')
+    # Display score of the password
+    if password:
+        result = zxcvbn(password)
+        st.write(f"Password strength: {result['score']}/4")
+        st.write(f"Suggestions: {result['feedback']['suggestions']}")
     openai_api_key = st.text_input("OpenAI API Key", type='password')
     pandasai_api_key = st.text_input("PandasAI API Key", type='password')
 
+
     if st.button("Sign Up"):
-        if username and password and (openai_api_key or pandasai_api_key):
+        if password and (result:=zxcvbn(password))['score'] < 3:
+            st.error(f"Password is too weak. {result['feedback']['suggestions']}")
+        elif username and password and (openai_api_key or pandasai_api_key):
             if auth.signup(username, password):
                 st.success("Account created successfully!")
                 st.write("Please login to access the chatbot.")
